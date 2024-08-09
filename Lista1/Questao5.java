b8cbac68dcb74039ac383b442e6f3ed80c0a3cc3
Alan Michel <130312363+AlanMichel30@users.noreply.github.com>
2024-08-09 20:10:56 -0300

    Update Questao5.java

--- Lista1/Questao5.java
+++ Lista1/Questao5.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Questao5 {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Digite o salário mínimo: ");
         double salarioMinimo = s.nextDouble();
 
