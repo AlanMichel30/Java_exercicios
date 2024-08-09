4f61f6e987488956bd3e3cac26587448020e991a
Alan Michel <130312363+AlanMichel30@users.noreply.github.com>
2024-08-09 20:12:23 -0300

    Add files via upload

---
+++ Lista2/Questao6.java
@@ -0,0 +1,15 @@
+
+public class Questao6 {
+    public static void main(String[] args) {
+
+        boolean  x = false; // escolher digite false ou ture
+
+        if(x){
+            System.out.print("VERDADEIROS");
+        } else {
+            System.out.print("FALSO");
+        }
+
+
+    }
+}
