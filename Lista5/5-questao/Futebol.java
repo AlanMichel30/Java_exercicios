a6a1508aaf0760deecd78df1a102affb5892dcd1
Alan Michel <130312363+AlanMichel30@users.noreply.github.com>
2024-09-06 18:54:01 -0300

    Update Futebol.java

--- Lista5/5-questao/Futebol.java
+++ Lista5/5-questao/Futebol.java
@@ -1,23 +1,16 @@
 public class Futebol extends Esporte{
     private String time;
-    private int numero_de_camisa;
+
     private int anos;
     private String Nascimento;
 
-    public Futebol(String nome, int numero_de_jogadores, double tempo_de_jogo, String time, int numero_de_camisa, int anos, String nascimento) {
+    public Futebol(String nome, int numero_de_jogadores, double tempo_de_jogo, String time, int anos, String nascimento) {
         super(nome, numero_de_jogadores, tempo_de_jogo);
         this.time = time;
-        this.numero_de_camisa = numero_de_camisa;
         this.anos = anos;
         Nascimento = nascimento;
     }
 
-    public Futebol(String time, int numero_de_camisa, int anos, String nascimento) {
-        this.time = time;
-        this.numero_de_camisa = numero_de_camisa;
-        this.anos = anos;
-        Nascimento = nascimento;
-    }
 
     public String getTime() {
         return time;
@@ -27,13 +20,6 @@ public class Futebol extends Esporte{
         this.time = time;
     }
 
-    public int getNumero_de_camisa() {
-        return numero_de_camisa;
-    }
-
-    public void setNumero_de_camisa(int numero_de_camisa) {
-        this.numero_de_camisa = numero_de_camisa;
-    }
 
     public int getAnos() {
         return anos;
@@ -49,5 +35,15 @@ public class Futebol extends Esporte{
 
     public void setNascimento(String nascimento) {
         Nascimento = nascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Futebol{" +
+                "time='" + time + '\'' +
+                ", anos=" + anos +
+                ", Nascimento='" + Nascimento + '\'' +
+                super.toString()+
+                '}';
     }
 }
